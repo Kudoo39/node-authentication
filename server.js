e877998cfa6c4632679eba022ef9533a9db7cdb8
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-09-07 19:17:29 +0300

    registering users

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post("/register", async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword,
     });
-  } catch {}
+
+    res.redirect("/login");
+  } catch (error) {
+    res.redirect("/register");
+  }
 });
 
 app.get("/", (req, res) => {
