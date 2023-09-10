628dd90b8bd529c23152cc78ea56b8fc30a41659
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-09-10 15:00:26 +0300

    setting up app for authentication

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const initializePassport = require("./authentication");
+const flash = require("express-flash");
+const session = require("express-session");
+
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email)
+);
 
 const users = [];
 
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.post("/register", async (req, res) => {
   try {
