ca29273265b5afe8a22d6c6783c6d2426def8718
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-09-10 16:27:45 +0300

    working one

--- authentication.js
+++ authentication.js
@@ -2,7 +2,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 const { serializeUser } = require("passport");
 
-function initialize(passport) {
+function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUsers = async (email, password, done) => {
     const user = getUserByEmail(email);
     if (user == null) {
@@ -26,8 +26,10 @@ function initialize(passport) {
   passport.use(
     new LocalStrategy({ usernameField: "email" }, authenticateUsers)
   );
-  passport.serializeUser((user, done) => {});
-  passport.deserializeUser((id, done) => {});
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id));
+  });
 }
 
 module.exports = initialize;
